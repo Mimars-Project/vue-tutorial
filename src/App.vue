a12013503dbc3bf2111b863932f62dfafac11557
Mimars-Project <nzbloxicoot@gmail.com>
2022-03-03 21:22:34 +1300

    image and chatbot added to website

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,18 @@ const convertToSpaces = require('./convertToSpaces');
 
 let test = convertToSpaces("hello?world");
 console.log(test);
+
+window.watsonAssistantChatOptions = {
+  integrationID: "4aa8e599-2c29-4618-a21d-fc3801b655c9", // The ID of this integration.
+  region: "au-syd", // The region your integration is hosted in.
+  serviceInstanceID: "f97c5424-5722-4e12-b033-87f7e932e147", // The ID of your service instance.
+  onLoad: function(instance) { instance.render(); }
+};
+setTimeout(function(){
+  const t=document.createElement('script');
+  t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+  document.head.appendChild(t);
+});
 </script>
 
 <style>
